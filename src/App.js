0bef3a05b127661904fc1fa73f795db7aaf9193e
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2023-01-24 15:55:38 -0800

    Add Header component and modify App component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,13 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 import axios from "axios";
-import PetsittersList from "./components/PetsittersList";
-
-import NavScrollExample from "./components/NavBar";
-
-// function App() {
-//   const [petsittersList, setPetsitterList] = useState([]);
-//   const [selectedPetsitter, setSelectedPetsitter] = useState(null);
-
-//   const API_URL = "http://localhost:8000/api/petsitters/";
-
-//   const fetchAllPetsitters = () => {
-//     axios
-//       .get(API_URL)
-//       .then((res) => {
-//         console.log(res);
-//         const petsittersAPIResCopy = res.data.map((petsitter) => {
-//           return {
-//             id: petsitter.pk,
-//             name: petsitter.name,
-//             email: petsitter.email,
-//             zipcode: petsitter.zipcode,
-//             city: petsitter.city,
-//             isAvailableHelp: petsitter.is_available_help,
-//             petType: petsitter.pet_type,
-//           };
-//         });
-//         console.log(petsittersAPIResCopy);
-//         setPetsitterList(petsittersAPIResCopy);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
-
-//   useEffect(fetchAllPetsitters, []);
-
-//   const loadPetsitterOnClick = (petsitter) => {
-//     console.log("load petsitter function called");
-//     setSelectedPetsitter(petsitter);
-//   };
-
-//   return (
-//     <div className="App">
-//       <header className="App-header"></header>
-//       {/* <OffcanvasExample></OffcanvasExample> */}
-//       <NavScrollExample></NavScrollExample>
-//       <h1>Welcome to the PetCareExchange!</h1>
-//       <h2>Select PetSitter</h2>
-//       <PetsittersList
-//         petsitters={petsittersList}
-//         loadPetsitterOnClick={loadPetsitterOnClick}
-//       />
-//       {/* <h2>Selected Petsitter</h2>
-//       {selectedPetsitter
-//         ? `${selectedPetsitter.name} - ${selectedPetsitter.email}`
-//         : "Click on the petsitter to get more info"} */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import ReactDOM from "react-dom/client";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Petsitter from "./components/Petsitter";
-import NewPetsitterForm from "./components/NewPetsitterForm";
+import Header from "./pages/Header";
 import Home from "./pages/Home";
+import NewPetsitterForm from "./components/NewPetsitterForm";
 import NoPage from "./pages/NoPage";
-import Layout from "./pages/Layout";
+import PetsittersList from "./components/PetsittersList";
+import Petsitter from "./components/Petsitter";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 
@@ -112,7 +50,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Header />}>
           <Route index element={<Home />} />
           <Route
             path="petsitters"
---
+++ src/pages/Header.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { GiHollowCat } from "react-icons/gi";
+import { GrSearch } from "react-icons/gr";
+import { LinkContainer } from "react-router-bootstrap";
+import { Outlet } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <GiHollowCat size="30px" color="#38bac4"></GiHollowCat>
+            PetCareExchange
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/petsitters">
+              <Nav.Link>Petsitters</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/petsitter">
+              <Nav.Link>Petsitter</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/petsitteraccount">
+              <Nav.Link>Become Pet sitter</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/">
+              <Nav.Link>
+                <GrSearch size="20px"></GrSearch>Search Petsitter
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/signup">
+              <Nav.Link>SignUp</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/signin">
+              <Nav.Link>SignIn</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+};
+
+export default Header;
+
+// const Header = () => {
+//   return (
+//     <>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/petsitters">Petsitters</Link>
+//           </li>
+//           <li>
+//             <Link to="/petsitter">Petsitter</Link>
+//           </li>
+//           <li>
+//             <Link to="/petsitteraccount">Become petsitter</Link>
+//           </li>
+//           <li>
+//             <Link to="/signup">SignUp</Link>
+//           </li>
+//           <li>
+//             <Link to="/signin">SignIn</Link>
+//           </li>
+//         </ul>
+//       </nav>
+
+//       <Outlet />
+//     </>
+//   );
+// };
+
+// export default Header;
